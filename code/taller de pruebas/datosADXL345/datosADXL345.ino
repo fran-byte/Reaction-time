396a96a10ab204f4b7abcd28a199be701f01ceaf
fran-byte <fran-byte@hotmail.com>
2022-02-01 00:34:38 +0100

    Update datosADXL345.ino

--- code/taller de pruebas/datosADXL345/datosADXL345.ino
+++ code/taller de pruebas/datosADXL345/datosADXL345.ino
@@ -2,7 +2,15 @@
 #include <Wire.h>
 #include <SparkFun_ADXL345.h> 
 
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
 ADXL345 adxl = ADXL345();
+
+const unsigned long intervaloLecturaTemp = 200; // leeremos cada ... milisegundos  (Evito usar la función delay)
+unsigned long tiempoAnterior = 0; // almacena la ultima vez que se lanzo nuestro evento
 
 void setup() 
 {
@@ -17,11 +25,15 @@ void setup()
 void loop() 
 {
    //leer los valores e imprimirlos
-   int x, y, z;
-   adxl.readAccel(&x, &y, &z);  
-   Serial.print(x);
-   Serial.print(", ");
-   Serial.print(y);
-   Serial.print(", ");
-   Serial.println(z); 
+   unsigned long tiempoActual = millis();
+  if (tiempoActual - tiempoAnterior >= intervaloLecturaTemp)  {
+       int x, y, z;
+       adxl.readAccel(&x, &y, &z);  
+       Serial.print(x);
+       Serial.print(", ");
+       Serial.print(y);
+       Serial.print(", ");
+       Serial.println(z); 
+       tiempoAnterior = tiempoActual;
+   }
 }
